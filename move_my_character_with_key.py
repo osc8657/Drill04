d83642588c0961b54520163c8b70e88a6cfab1e7
osc8657 <osc8657@naver.com>
2023-09-26 01:35:40 +0900

    test 1

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,2 +1,46 @@
 from pico2d import *
 
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.jpeg')
+
+def handle_events():
+    global running
+
+    global x, y
+
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                x += 10
+            elif event.key == SDLK_LEFT:
+                x -= 10
+            elif event.key == SDLK_UP:
+                y += 10
+            elif event.key == SDLK_DOWN:
+                y -= 10
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+running = True
+x = TUK_WIDTH // 2
+y = TUK_HEIGHT // 2
+frame = 0
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+close_canvas()
+
+
