cd91211e9c028cccaa41d87b48812c16f4ef3623
osc8657 <osc8657@naver.com>
2023-09-26 01:57:16 +0900

    change transparent sheet

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,7 +3,7 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.jpeg')
+character = load_image('animation_sheet.png')
 
 def handle_events():
     global running
@@ -34,8 +34,10 @@ frame = 0
 
 while running:
     clear_canvas()
+
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
